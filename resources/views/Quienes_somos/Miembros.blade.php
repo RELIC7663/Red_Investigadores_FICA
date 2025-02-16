092a2803c8ce5e8253ee6293fea3b223ab403e0b
itsThest <barodrigueze@utn.edu.ec>
2025-02-15 23:37:34 -0500

    Implementación: Miembros y reeestructuracion de vista miembros.blade.php

--- resources/views/Quienes_somos/Miembros.blade.php
+++ resources/views/Quienes_somos/Miembros.blade.php
@@ -33,134 +33,124 @@
                 @php
                     $miembros = [
                         [
-                            'nombre' => 'PhD. Daisy Elizabeth Imbaquingo Esparza',
-                            'email' => 'docente@utn.edu.ec',
+                            'nombre' => 'Daisy Elizabeth Imbaquingo Esparza',
+                            'email' => 'deimbaquingo@utn.edu.ec',
                             'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
                             'orcid' => 'https://orcid.org/0000-0002-6412-6257',
                         ],
                         [
-                            'nombre' => 'PhD. Diego Ernesto Echeverría Jurado',
-                            'email' => 'wresparza@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-0492-9979',
+                            'nombre' => ' Diego Ernesto Echeverría Jurado',
+                            'email' => 'decheverria@cenace.gob.ec',
+                            'imagen' => asset('images/foto_miembros/deej_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0002-1743-9234',
                         ],
                         [
-                            'nombre' => 'PhD. Julio Esteban Guerra Massón',
-                            'email' => 'jeguera@utn.edu.ec',
+                            'nombre' => 'Julio Esteban Guerra Massón',
+                            'email' => 'jeguerra@utn.edu.ec',
                             'imagen' => 'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/jeguerra.png',
                             'orcid' => 'https://orcid.org/0000-0001-5278-719X',
                         ],
                         [
-                            'nombre' => 'PhD. Andrés Navarro Cadavid',
+                            'nombre' => ' Andrés Navarro Cadavid',
                             'email' => 'anavarr@icesi.edu.co',
-                            'imagen' => 'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/andresnavarro.jpg',
+                            'imagen' => asset('images/foto_miembros/anc_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0002-4154-2119',
+                        ],
+                        [
+                            'nombre' => 'Verónica Isolina Massón Fiallos',
+                            'email' => 'vmasson@utn.edu.ec',
+                            'imagen' =>asset('images/foto_miembros/vimf_miembro.png'),
+                            'orcid' => 'https://orcid.org/xxxx-xxxx-xxxx-xxxx',
+                        ],
+                        [
+                            'nombre' => ' Andres Santiago Martínez León',
+                            'email' => 'xxxx@utn.edu.ec',
+                            'imagen' => asset( 'images/foto_miembros/asml_miembro.png'),
+                            'orcid' => 'https://orcid.org/xxxx-xxxx-xxxx-xxxx',
+                        ],
+                        [
+                            'nombre' => ' Marcia Catalina Ramirez Galarraga',
+                            'email' => 'mcramirez@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/mcrg_miembro.png'),
+
+                            'orcid' => 'https://orcid.org/xxxx-xxxx-xxxx-xxxx',
+                        ],
+                        [
+                            'nombre' => ' Albert Giovanny Espinal Santana',
+                            'email' => 'aespinal@espol.edu.ec',
+                            'imagen' => asset('images/foto_miembros/ages_miembro.png'),
+
+                            'orcid' => 'https://orcid.org/0000-0003-2619-2752',
+                        ],
+                        [
+                            'nombre' => 'Edwin Giannine Valarezo Añazco',
+                            'email' => 'edgivala@espol.edu.ec',
+                            'imagen' => asset('images/foto_miembros/egva_miembro.png'),
+
+                            'orcid' => 'https://orcid.org/0000-0003-0077-8528',
+                        ],
+                        [
+                            'nombre' => 'Hernán Adolfo Samaniego Armijos',
+                            'email' => 'xxxx@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/hasa_miembro.png'),
+
+                            'orcid' => 'https://orcid.org/xxxx-xxxx-xxxx-xxxx',
+                        ],
+                        [
+                            'nombre' => 'Diego Ramiro Villalba Calderón',
+                            'email' => 'esvillalbadiego@yahoo.com',
+                            'imagen' => asset('images/foto_miembros/drvc_miembro.png'),
+
+                            'orcid' => 'https://orcid.org/0000-0002-3794-6604',
+                        ],
+                        [
+                            'nombre' => 'Jaime Alcides Meza Hormaza',
+                            'email' => 'jaime.meza@utm.edu.ec',
+                            'imagen' => asset('images/foto_miembros/jamh_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0002-8279-5630',
+                        ],
+                        [
+                            'nombre' => 'Julio Joffre Barzola Monteses',
+                            'email' => 'julio.barzolam@ug.edu.ec',
+                            'imagen' => asset('images/foto_miembros/jjbm_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0003-2732-979X',
+                        ],
+                        [
+                            'nombre' => 'Willim Ricardo Esparza Encalada',
+                            'email' => 'wresparza@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/wree_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0003-0492-9979',
+                        ],
+                        [
+                            'nombre' => 'Carlos Nolasco Mafla Yepez',
+                            'email' => 'cnmafla@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/cnmy_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0003-3704-8676',
+                        ],
+                        [
+                            'nombre' => 'Robert Mauricio Valencia Chapi',
+                            'email' => 'rmvalencia@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/rmvc_miembro.png'),
+                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
+                        ],
+                        [
+                            'nombre' => 'Brizeida Nohemi Gamez Aparicio',
+                            'email' => 'bngamez@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/bnga_miembro.png'),
+                            'orcid' => 'https://orcid.org/xxxx-xxxx-xxxx-xxxx',
+                        ],
+                        [
+                            'nombre' => 'Cosme Damian Mejia Echeverria',
+                            'email' => 'cdmejia@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/cdme_miembro.png'),
                             'orcid' => 'https://orcid.org/0000-0001-6961-2644',
                         ],
                         [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
+                            'nombre' => 'Carlos Alberto Vasquez Ayala',
+                            'email' => 'cavasquez@utn.edu.ec',
+                            'imagen' => asset('images/foto_miembros/cava_miembro.png'),
+                            'orcid' => 'https://orcid.org/xxxx-xxxx-xxxx-xxxx',
                         ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' => 'https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg',
-
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ],
-                        [
-                            'nombre' => 'PhD. Verónica Isolina Massón Fiallos',
-                            'email' => 'vmasson@utn.edu.ec',
-                            'imagen' =>
-                                'https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/veronicamasson.jpg',
-                            'orcid' => 'https://orcid.org/0000-0003-1977-2118',
-                        ]
                     ];
                 @endphp
 
