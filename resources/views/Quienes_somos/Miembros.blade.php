2126faadf41c347350910c149927c7ec60314133
RELIC7663 <alexispepinosa28@gmail.com>
2025-02-16 02:11:20 -0500

    dev FAVICON

--- resources/views/Quienes_somos/Miembros.blade.php
+++ resources/views/Quienes_somos/Miembros.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Miembros - Red Investigadores</title>
 
-
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
 </head>
