4887cc48d5b62e249aa2393ed9d1fdeabf7ef474
RELIC7663 <alexispepinosa28@gmail.com>
2025-02-16 01:58:04 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -315,6 +315,7 @@
                 height: 2px;
                 background-color: #DC0000;
             }
+        }
     </style>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
