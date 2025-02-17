449c14e37977609e94e2d9d734ccac534f3bbf7f
itsThest <barodrigueze@utn.edu.ec>
2025-02-16 20:20:35 -0500

    Implementacion: informacion en directiva.blade.php

--- resources/views/Quienes_somos/Directiva.blade.php
+++ resources/views/Quienes_somos/Directiva.blade.php
@@ -30,51 +30,51 @@
             <h2 class="titulo">Presidente</h2>
             <div class="tarjeta-miembro">
                 <div class="fondo-img">
-                    <img src="images/foto_miembros/presidente.png" alt="Presidente">
+                    <img src="https://software.utn.edu.ec/wp-content/uploads/2024/05/daisy.jpg" alt="Presidente">
                 </div>
-                <h5>xxxx-xxxx-xxxx-xxxx</h5>
-                <p>Email: xxxx@utn.edu.ec</p>
-                <a href="https://orcid.org/xxxx-xxxx-xxxx-xxxx" class="orcid-btn" target="_blank">ORCID</a>
+                <h5>Daisy Elizabeth Imbaquingo Esparza</h5>
+                <p>Email: deimbaquingo@utn.edu.ec</p>
+                <a href="https://orcid.org/0000-0002-6412-6257" class="orcid-btn" target="_blank">ORCID</a>
             </div>
 
             <h2 class="titulo">Vicepresidente</h2>
             <div class="tarjeta-miembro">
                 <div class="fondo-img">
-                    <img src="images/foto_miembros/vicepresidente.png" alt="Vicepresidente">
+                    <img src="{{ asset('images/foto_miembros/deej_miembro.png') }}" alt="Vicepresidente">
                 </div>
-                <h5>xxxx-xxxx-xxxx-xxxx</h5>
-                <p>Email: xxxx@cenace.gob.ec</p>
-                <a href="https://orcid.org/xxxx-xxxx-xxxx-xxxx" class="orcid-btn" target="_blank">ORCID</a>
+                <h5>Diego Ernesto Echeverría Jurado</h5>
+                <p>Email: decheverria@cenace.gob.ec</p>
+                <a href="https://orcid.org/0000-0002-1743-9234" class="orcid-btn" target="_blank">ORCID</a>
             </div>
 
             <h2 class="titulo">Secretario</h2>
             <div class="tarjeta-miembro">
                 <div class="fondo-img">
-                    <img src="images/foto_miembros/secretario.png" alt="Secretario">
+                    <img src="https://investigacion.utn.edu.ec/wp-content/uploads/2024/07/jeguerra.png" alt="Secretario">
                 </div>
-                <h5>xxxx</h5>
-                <p>Email: xxxx@utn.edu.ec</p>
+                <h5>Julio Esteban Guerra Massón</h5>
+                <p>Email: jeguerra@utn.edu.ec</p>
                 <a href="https://orcid.org/0000-0001-5278-719X" class="orcid-btn" target="_blank">ORCID</a>
             </div>
 
             <h2 class="titulo">Primer Vocal</h2>
             <div class="tarjeta-miembro">
                 <div class="fondo-img">
-                    <img src="images/foto_miembros/primer_vocal.png" alt="Primer Vocal">
+                    <img src="{{ asset('images/foto_miembros/hasa_miembro.png') }}" alt="Primer Vocal">
                 </div>
-                <h5>xxx</h5>
+                <h5>Hernán Adolfo Samaniego Armijos</h5>
                 <p>Email: xxxx@icesi.edu.co</p>
-                <a href="https://orcid.org/xxxx-xxxx-xxxx-xxxx" class="orcid-btn" target="_blank">ORCID</a>
+                <a href="https://orcid.org/0000-0001-6161-7002" class="orcid-btn" target="_blank">ORCID</a>
             </div>
 
             <h2 class="titulo">Segundo Vocal</h2>
             <div class="tarjeta-miembro">
                 <div class="fondo-img">
-                    <img src="images/foto_miembros/segundo_vocal.png" alt="Segundo Vocal">
+                    <img src="{{ asset('images/foto_miembros/jjbm_miembro.png') }}" alt="Segundo Vocal">
                 </div>
-                <h5>xxxx</h5>
-                <p>Email: xxxx@utn.edu.ec</p>
-                <a href="https://orcid.org/xxxx-xxxx-xxxxx-xxxxx" class="orcid-btn" target="_blank">ORCID</a>
+                <h5>Julio Joffre Barzola Monteses</h5>
+                <p>Email: julio.barzolam@ug.edu.ec</p>
+                <a href="https://orcid.org/0000-0003-2732-979X" class="orcid-btn" target="_blank">ORCID</a>
             </div>
         </div>
     </div>
